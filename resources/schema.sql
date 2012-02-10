38d41c81b2335698da38689f723f7503f004a69b
Graylin Kim <graylin.kim@gmail.com>
2012-02-10 14:53:23 -0500

    Update the schema file to reflect new components.

--- resources/schema.sql
+++ resources/schema.sql
@@ -88,3 +88,43 @@ CREATE TABLE unsubscribe (
     event_id int(10) unsigned PRIMARY KEY,
     FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
+
+
+CREATE TABLE `summary` (
+    `mailing_id` int(10) NOT NULL,
+    `instance` varchar(32) NOT NULL,
+    `install_class` varchar(8) NOT NULL,
+    `event` varchar(50) NOT NULL,
+    `category` varchar(255) NOT NULL,
+    `count` int(10) NOT NULL,
+    `dt_first` datetime NOT NULL,
+    `dt_last` datetime NOT NULL,
+    UNIQUE KEY (`mailing_id`,`instance`,`install_class`,`event`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DELIMITER |
+
+    -- On every event insert, sync with the summary table
+    DROP TRIGGER IF EXISTS insert_stats_table |
+    CREATE TRIGGER insert_stats_table AFTER INSERT ON event
+        FOR EACH ROW BEGIN
+
+            -- If queue_id=0 we don't process the event
+            -- we need to exclude these from summaries.
+            IF NEW.queue_id <> 0 THEN
+
+                INSERT INTO summary(
+                    mailing_id, instance, install_class, event, category, count, dt_first, dt_last
+                ) VALUES (
+                    NEW.mailing_id, NEW.instance, NEW.install_class, NEW.event, NEW.category, 1, NEW.timestamp, NEW.timestamp
+                ) ON DUPLICATE KEY UPDATE
+                    count = ( count +1 ),
+                    dt_last = IF( dt_last < NEW.timestamp, NEW.timestamp, dt_last ),
+                    dt_first = IF( dt_first > NEW.timestamp, NEW.timestamp, dt_first );
+
+            END IF;
+        END
+    |
+
+DELIMITER ;
