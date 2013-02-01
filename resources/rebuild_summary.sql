0d762d03c3fb9c40ff0a9c1b40c5d904722fabcf
Graylin Kim <graylin.kim@gmail.com>
2013-02-01 11:55:03 -0500

    Don't include test mailing events in the summary table.

--- resources/rebuild_summary.sql
+++ resources/rebuild_summary.sql
@@ -1,4 +1,4 @@
-
+-- Just change the dt_created time to adjust the window that is rebuilt
 REPLACE INTO summary (
   SELECT mailing_id, instance_name, install_class, event_type, category, count(*), min(dt_created), max(dt_created)
   FROM archive JOIN (
@@ -7,11 +7,11 @@ REPLACE INTO summary (
       JOIN instance ON message.instance_id=instance.id
       JOIN archive ON archive.message_id=message.id
     WHERE archive.dt_created > '2012-07-24 12:00:00'
-
       AND archive.queue_id != 0
       AND mailing_id != 0
       AND instance.servername != ''
     ) AS affected_messages USING (message_id)
-  WHERE queue_id != 0
+  WHERE queue_id != 0 AND is_test != 0
   GROUP BY instance_name, install_class, mailing_id, event_type
 );
+
